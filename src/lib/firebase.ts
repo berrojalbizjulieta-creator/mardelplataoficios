08915eb5631b4ffc3c033b2feafc5aa6568de7fb
Agustín Arturo Giardino <agustinarturogiardino@gmail.com>
2025-12-18 00:17:34 +0000

    probemos a ver como entro a ala ocnsola de este neuvo proyecot..?

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // src/lib/firebase.ts
 import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
 import { getAuth, Auth } from "firebase/auth";
@@ -5,13 +6,18 @@ import { getFirestore, Firestore } from "firebase/firestore";
 import { getStorage, FirebaseStorage } from "firebase/storage";
 import { getFunctions, Functions } from 'firebase/functions';
 
+// Esta es la configuración de tu NUEVO y único proyecto de Firebase.
 const firebaseConfig = {
-  apiKey: "AIzaSyAkwEuIqBsIIY_cKmrAkSQhVVUhdrn1xhc",
-  authDomain: "studio-4820039016-5ae38.firebaseapp.com",
-  projectId: "studio-4820039016-5ae38",
-  storageBucket: "studio-4820039016-5ae38.firebasestorage.app",
-  messagingSenderId: "821979814317",
-  appId: "1:821979814317:web:abd23395565fe4e58f6924"
+  apiKey: "AIzaSyA43q5nZJq-g_btpQhsmEw3i7s52Gjl1lQ",
+  authDomain: "studio-4820039016-83344.firebaseapp.com",
+  // ------------------------------------------------------------------
+  // ¡ESTE ES EL ID DE TU NUEVO PROYECTO! 
+  // Búscalo en console.firebase.google.com para acceder a su consola.
+  // ------------------------------------------------------------------
+  projectId: "studio-4820039016-83344",
+  storageBucket: "studio-4820039016-83344.appspot.com",
+  messagingSenderId: "54230232537",
+  appId: "1:54230232537:web:c02d8479e95195ed8422aa"
 };
 
 let app: FirebaseApp;

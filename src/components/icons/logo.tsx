9fdaf34642e28cea3887fbfaf73a8bbffa0da762
Agustín Arturo Giardino <agustinarturogiardino@gmail.com>
2026-02-05 09:16:20 +0000

    quedo horrendo ahi un svg pero la llave tiene que sser del color de cerr

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -5,18 +5,15 @@ export function Logo(props: any) {
   return (
     <div className="flex items-center gap-2">
       <svg
+        xmlns="http://www.w3.org/2000/svg"
         width="32"
         height="32"
-        viewBox="0 0 512 512"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
         className="text-primary"
         {...props}
       >
-        <path
-          d="M368.6 112.5C337.1 69.4 286.4 48 223.1 48 135.5 48 64 119.5 64 207.1s71.5 159.1 159.1 159.1c46.7 0 89.2-18.5 119.8-48.9l89.3 89.3c15.1 15.1 39.5 15.1 54.6 0s15.1-39.5 0-54.6l-85.2-85.2c16.3-29.4 25.5-63.5 25.5-99.6.1-50.6-26.6-92.4-83.6-114.3zM223.1 336.2c-70.9 0-128.1-57.2-128.1-128.1s57.2-128.1 128.1-128.1 128.1 57.2 128.1 128.1-57.2 128.1-128.1 128.1zM464 368h-48v-48h-48v-48h-48v-48h144v144z"
-          fill="currentColor"
-        />
+        <path d="M12.793 2.207a1 1 0 0 0-1.414 0l-5 5a1 1 0 0 0 0 1.414l5 5a1 1 0 0 0 1.414-1.414L9.414 8.828 12.243 6H19a1 1 0 1 0 0-2h-6.757l2.828-2.828a1 1 0 0 0 0-1.414zM10 14a4 4 0 1 1 0 8 4 4 0 0 1 0-8zm0 2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z" />
       </svg>
       <div className="text-xl font-bold font-headline">
         <span className="text-primary">Cerrajeros</span>
